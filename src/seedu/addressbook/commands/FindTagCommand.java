59ba5b6b280acec9493995f87361d23590646bb6
Chuang Cheng Heng <mukevin66@gmail.com>
2018-02-13 16:39:37 +0800

    FindTagCommand.java: Implement find by tags.

--- src/seedu/addressbook/commands/FindTagCommand.java
+++ src/seedu/addressbook/commands/FindTagCommand.java
@@ -6,7 +6,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.tag.Tag;
 
 /**
  * Finds and lists all persons in address book whose tags contains any of the argument keywords.
@@ -22,9 +24,15 @@ public class FindTagCommand extends Command {
             + "Example: " + COMMAND_WORD + " friends owesMoney";
 
     private final Set<String> keywords;
+    private final Set<Tag> keyTags;
 
-    public FindTagCommand(Set<String> keywords) {
+    public FindTagCommand(Set<String> keywords) throws IllegalValueException{
         this.keywords = keywords;
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String keyword : keywords) {
+            tagSet.add(new Tag(keyword));
+        }
+        keyTags = tagSet;
     }
 
     /**
@@ -36,21 +44,21 @@ public class FindTagCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeyword(keywords);
+        final List<ReadOnlyPerson> personsFound = getPersonsWithTagContainingAnyKeyword(keyTags);
         return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
     }
 
     /**
      * Retrieves all persons in the address book whose tags contain some of the specified keywords.
      *
-     * @param keywords for searching
+     * @param keyTags for searching
      * @return list of persons found
      */
-    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
+    private List<ReadOnlyPerson> getPersonsWithTagContainingAnyKeyword(Set<Tag> keyTags) {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
-            final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
-            if (!Collections.disjoint(wordsInName, keywords)) {
+            final Set<Tag> tagsOfPerson = person.getTags().toSet();
+            if (!Collections.disjoint(tagsOfPerson, keyTags)) {
                 matchedPersons.add(person);
             }
         }
